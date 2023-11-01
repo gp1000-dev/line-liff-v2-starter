4456544d6daaa34dd1382922350f96b766825403
gp-n-suzuki <nobuaki_s@gp1000.jp>
2023-11-01 17:06:48 +0900

    test

--- index.js
+++ index.js
@@ -5,9 +5,35 @@ document.addEventListener("DOMContentLoaded", function() {
   liff
     .init({ liffId: process.env.LIFF_ID })
     .then(() => {
-        console.log("Success! you can do something with LIFF API here.")
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+        console.log("Success! you can do something with LIFF API here.");
+        document.getElementById('text').innerHTML = 'Success! you can do something with LIFF API here.';
+
+        const contact_name = 'test';
+        const email = 'test2';
+        const body = 'test3';
+
+        const api_url = "https://script.google.com/macros/s/AKfycbytDrJaBIwmuuutJYf37k5h18fROp9dyvF8hU2stJs3bP-oxgzr64MDXAFSJSYU7ywdvA/exec";
+        fetch(api_url, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: encodeURI(`name=${contact_name}&email=${email}&body=${body}`),
+        })
+            .then((response) => {
+                if (response.ok) console.log('okです！'); 
+                response.text().then((text) => {
+                    console.log(response);
+                    console.log(text);
+                    alert(text);
+                });
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
+        
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 });
